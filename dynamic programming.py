256bb93a8db6e04fcc15b4afca5998e85df8affd
dong-zhan <41976575+dong-zhan@users.noreply.github.com>
2020-08-07 16:33:38 -0700

    Update dynamic programming.py

--- dynamic programming.py
+++ dynamic programming.py
@@ -1,69 +1,83 @@
 #reference: https://towardsdatascience.com/reinforcement-learning-rl-101-with-python-e1aa0d37d43b
 
-#this is a good practise of dynamic programming - value iteration
-#this implementation is just a simple extension of the one in the reference.
-#in this implementation, the reward can be dynamic!
-
 def imps():
 	global np, random
 	import numpy as np
 	import random
-		
-def Parameters():
-	global gamma, gridSizeX, gridSizeY, gridSizeX_1, gridSizeY_1, actions, numIterations, weight, terminationStates, rewards, defaultReward
-	gamma = 1 # discounting rate
-	gridSizeX = 4
-	gridSizeY = 4
-	gridSizeY_1 = gridSizeY - 1
-	gridSizeX_1 = gridSizeX - 1
-	actions = np.array([[-1, 0], [1, 0], [0, 1], [0, -1]])	
-	numIterations = 1000
-	weight = 1./len(actions)
 	
-	defaultReward = -1
-	terminationStates = np.array([(0,0), (3,3)])
-	rewards = np.array([0, 0])
+def swapColumn(arr, c0, c1):
+	temp = np.copy(arr[:, c0])
+	arr[:, c0] = arr[:, c1]
+	my_array[:, c1] = temp
 	
-def GetIndexInTerminationStates(pos):
-	global terminationStates
-	for i, p in enumerate(terminationStates):
-		comparison = pos == p
-		if comparison.all():
-			return i
-	return -1
+def copyColumn(arr, to, fr):
+	arr[:, to] = arr[:, fr]
 	
-def getTargetPosAndReward(initPos, action):
-	global rewards, gridSizeX_1, gridSizeY_1
-	idx = GetIndexInTerminationStates(initPos)
-	if not idx == -1:
-		return initPos, rewards[idx]
-			
-	return np.clip(initPos + action, [0,0], [gridSizeY_1, gridSizeX_1]), defaultReward
+def copyRow(arr, to, fr):
+	arr[to] = arr[fr]
+	
+def copyArrayContentToBorder(arr):
+	copyColumn(arr, 0, 1)
+	copyColumn(arr, gridSizeX+1, gridSizeX)
+	copyRow(arr, 0, 1)
+	copyRow(arr, gridSizeY+1, gridSizeY)
+	
+def fillArray(arr):		#for test
+	cnt = arr.shape[0] * arr.shape[1]
+	arr1 = arr.reshape(cnt)
+	for i in range(cnt):
+		arr1[i] = i
 
-def Initialization():
-	global valueMap, valueMapNext, gridSizeY, gridSizeX, states		#pingpong between valueMap and valueMapNext
-	valueMap = np.zeros((gridSizeY, gridSizeX))
-	valueMapNext = np.zeros((gridSizeY, gridSizeX))
-	states = [[y, x] for y in range(gridSizeY) for x in range(gridSizeX)]	
-	
+#reward is from initPos.
+def getTargetPosAndReward(initPos, action):
+	targetPos = (initPos[0], initPos[1])
+	reward = rewards[targetPos]
+	if reward == 0:
+		return targetPos, reward
+
+	targetPos = (initPos[0] + action[0], initPos[1] + action[1])
+	#reward = rewards[targetPos]
+	return targetPos, reward
+
 def swapValueMaps():
 	global valueMap, valueMapNext
 	tmpMap = valueMapNext
 	valueMapNext = valueMap
 	valueMap = tmpMap
 	
+def init():
+	global gamma, gridSizeX, gridSizeY, actions, numIterations, weight, rewards, valueMap, valueMapNext, states	
+	gamma = 1 # discounting rate
+	gridSizeX = 4
+	gridSizeY = 4
+	actions = np.array([[-1, 0], [1, 0], [0, 1], [0, -1]])	
+	numIterations = 1
+	weight = 1./len(actions)
+	
+	rewards = np.full((gridSizeY+2, gridSizeX+2), -1, dtype=float)  
+	rewards[1,1] = 0
+	rewards[4,4] = 0
+	copyArrayContentToBorder(rewards)
+
+	#make this 2 units bigger to avoid boundary check, every iteration, copy the content from appropriate row/columns
+	valueMap = np.zeros((gridSizeY+2, gridSizeX+2))
+	valueMapNext = np.zeros((gridSizeY+2, gridSizeX+2))
+	states = [[y, x] for y in range(1,gridSizeY+1) for x in range(1,gridSizeX+1)]		
+	
 def PolicyEvaluation():
 	global valueMap, copyValueMap, weight
 	for it in range(numIterations):
 		for state in states:
+			#print(state, rewards[state[0], state[1]])
 			weightedRewards = 0
 			for action in actions:
 				targetPos, reward = getTargetPosAndReward(np.array(state), action)
+				#print(targetPos, reward)
 				weightedRewards += weight*(reward+(gamma*valueMap[targetPos[0], targetPos[1]])) 				
 			valueMapNext[state[0], state[1]] = weightedRewards
 			
+		copyArrayContentToBorder(valueMapNext)
 		swapValueMaps()
 	
 	print(valueMap)
-			
 			
